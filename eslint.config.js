0560b6b4eb1c0941f8d3b0e18692a1ffe9e8d8f1
Iwantschenko <nazar.i@t-teck.com>
2026-03-04 09:36:40 +0200

    Init: add prettier and EsLint

---
+++ eslint.config.js
@@ -0,0 +1,125 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const angularPlugin = require('@angular-eslint/eslint-plugin');
+const angularTemplatePlugin = require('@angular-eslint/eslint-plugin-template');
+const angularTemplateParser = require('@angular-eslint/template-parser');
+const importPlugin = require('eslint-plugin-import');
+const globals = require('globals');
+const prettier = require('eslint-config-prettier');
+
+module.exports = defineConfig([
+  // TypeScript source files
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      '@angular-eslint': angularPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      // JS rules
+      semi: ['error', 'always'],
+      'prefer-const': 'error',
+      curly: ['error', 'all'],
+      'max-len': [
+        'error',
+        {
+          ignoreTemplateLiterals: true,
+          ignoreComments: true,
+        },
+      ],
+      'no-redeclare': ['error', { builtinGlobals: true }],
+      'no-console': 'warn',
+      'operator-linebreak': 'off',
+      'brace-style': ['error', '1tbs'],
+      'arrow-body-style': 'off',
+      'no-param-reassign': [
+        'error',
+        {
+          props: true,
+          ignorePropertyModificationsFor: ['state'],
+        },
+      ],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+        { blankLine: 'always', prev: 'directive', next: '*' },
+        { blankLine: 'always', prev: 'block-like', next: '*' },
+      ],
+
+      // TS rules
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      // Angular rules
+      '@angular-eslint/component-class-suffix': 'off',
+      '@angular-eslint/directive-class-suffix': 'off',
+      '@angular-eslint/no-input-rename': 'error',
+      '@angular-eslint/no-output-rename': 'error',
+      '@angular-eslint/use-lifecycle-interface': 'error',
+      '@angular-eslint/use-pipe-transform-interface': 'error',
+      '@angular-eslint/component-selector': [
+        'error',
+        { type: 'element', prefix: 'app', style: 'kebab-case' },
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        { type: 'attribute', prefix: 'app', style: 'camelCase' },
+      ],
+
+      // Import rules
+      'import/prefer-default-export': 'off',
+    },
+  },
+
+  // Angular HTML templates
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      '@angular-eslint/template': angularTemplatePlugin,
+    },
+    rules: {
+      '@angular-eslint/template/banana-in-box': 'error',
+      '@angular-eslint/template/no-negated-async': 'error',
+      '@angular-eslint/template/eqeqeq': 'error',
+    },
+  },
+
+  // Prettier — вимикає конфліктні правила форматування
+  prettier,
+
+  globalIgnores([
+    '**/eslint.config.js',
+    'dist',
+    'out-tsc',
+    '.angular',
+    'src/main.ts',
+  ]),
+]);
