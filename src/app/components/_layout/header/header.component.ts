8ce072720c73895a1a9383e17011787276267147
Iwantschenko <nazar.i@t-teck.com>
2026-03-09 13:05:30 +0200

    feat: add base styles, aliase , base element and layout of site

---
+++ src/app/components/_layout/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, NavbarComponent],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  favoritesCount = 0;
+  cartCount = 0;
+
+  isMenuOpen = false;
+}
