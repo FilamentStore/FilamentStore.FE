181acaf1e204e0dfb9c985d9cfc7da157801fc21
Iwantschenko <nazar.i@t-teck.com>
2026-03-09 14:05:11 +0200

    feat: base colors + add arrow-button component + more icons

--- src/app/components/_layout/footer/footer.component.ts
+++ src/app/components/_layout/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ArrowButtonComponent } from '../../arrow-button/arrow-button.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
+  imports: [ArrowButtonComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
