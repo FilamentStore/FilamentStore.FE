e8b71575507b7e74487cff04ac73995005bb37af
Iwantschenko <nazar.i@t-teck.com>
2026-03-29 18:52:28 +0300

    reafactor: config folder

--- src/app/helper/route/config.routes.ts
+++ src/app/helper/route/config.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import { ConfigPageComponent } from '@pages/crm/tabs/config-page/config-page.component';
+import { ConfigPageComponent } from '@app/pages/crm/configuration/config-page/config-page.component';
 import { configFeature } from '@store/config/config.reducer';
 import { ConfigEffects } from '@store/config/config.effects';
 
