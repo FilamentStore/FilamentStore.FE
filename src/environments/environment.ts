fce2b9e0999bd9481875e368767aa327189e7b1e
Iwantschenko <nazar.i@t-teck.com>
2026-05-17 20:13:19 +0300

    feat: new config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: '/wp-json/mystore/v1',
-  wpJsonUrl: '/wp-json',
+  apiUrl: 'https://ivory-cheetah-157641.hostingersite.com/wp-json/mystore/v1',
+  wpJsonUrl: 'https://ivory-cheetah-157641.hostingersite.com/wp-json',
 };
